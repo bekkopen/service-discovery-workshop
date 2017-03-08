7b412213936c0aaa8525df037400d98ceb22b2b7
Sveinung Dalatun <sdalatun@gmail.com>
2017-03-08 16:16:07 +0100

    Revert "Add config before installing nginx"
    This reverts commit 03bb0d65e65e6f352a34ec24f398bc21017cce9f.

--- provision/deploy_frontend.sh
+++ provision/deploy_frontend.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-sudo cp -r /vagrant/frontend/src/* /var/www/html
-sudo mkdir -p /etc/nginx/sites-available/
-sudo cp /vagrant/provision/nginx-site /etc/nginx/sites-available/default
-
 sudo apt-get install --yes nginx
+
+sudo cp -r /vagrant/frontend/src/* /var/www/html
+sudo cp /vagrant/provision/nginx-site /etc/nginx/sites-available/default
 
 sudo service nginx reload

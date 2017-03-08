03bb0d65e65e6f352a34ec24f398bc21017cce9f
Stian Mathiassen <stian.mathiassen@bekk.no>
2017-03-08 15:51:24 +0100

    Add config before installing nginx

--- provision/deploy_frontend.sh
+++ provision/deploy_frontend.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-get install --yes nginx
-
 sudo cp -r /vagrant/frontend/src/* /var/www/html
+sudo mkdir -p /etc/nginx/sites-available/
 sudo cp /vagrant/provision/nginx-site /etc/nginx/sites-available/default
+
+sudo apt-get install --yes nginx
 
 sudo service nginx reload

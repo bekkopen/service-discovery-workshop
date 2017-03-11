9e96eab1f8134a67db8515e20c98b2ed48aaf48a
Sveinung Dalatun <sdalatun@gmail.com>
2017-03-11 15:18:23 +0100

    Bugs

--- provision/deploy_service.sh
+++ provision/deploy_service.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cp /vagrant/service/target/service-*.jar /home/vagrant
+cp /vagrant/service/target/service-*.jar /home/ubuntu
 
-cp /vagrant/provision/service.sh /home/vagrant
+cp /vagrant/provision/service.sh /home/ubuntu
